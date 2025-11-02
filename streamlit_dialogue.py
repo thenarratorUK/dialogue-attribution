0096ceaa80c2d6a1b7d318721deab1662765acc7
thenarratorUK <david@thenarrator.co.uk>
2025-11-02 09:50:39 +0000

    Update streamlit_dialogue.py

--- streamlit_dialogue.py
+++ streamlit_dialogue.py
@@ -930,7 +930,7 @@ def extract_dialogue_from_docx(book_name, docx_path):
         # italics with spans
         for span, seg in extract_italic_spans(para):
         # Skip italics if enclosed by quotes (italics-only rule) or directly wrapped by quotes
-        is_, ie = span
+            is_, ie = span
         if _is_enclosed_by_quotes(para.text, is_, ie, seg) or _has_quotes_just_outside(span, para.text):
             continue
             items.append((span, seg))
